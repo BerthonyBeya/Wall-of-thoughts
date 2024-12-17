185614276e9c19fb0f8d93431b4489d797cd07e4
Berthony Beya <berthonybeyadropbox@gmail.com>
2024-12-17 10:21:57 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <div className="background-container">
-        <h1 className="thought">What's on your mind today?</h1>
+        <h1 className="thought">What's on your mind today, friend?</h1>
         <div className="textbox">
           <input type="textbox__text" />
         </div>
