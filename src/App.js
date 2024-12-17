f0aff0618a3fa52cbadaa1d1528ded0c5425e760
Berthony Beya <berthonybeyadropbox@gmail.com>
2024-12-17 09:54:56 +0530

    firt init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="background-container">
+        <h1 className="thought">What's on your mind today, nigga?</h1>
+        <input type="email" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
