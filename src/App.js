8242b44eb40cb8d5d160c5bb93eede0a2fd55aa5
Berthony Beya <berthonybeyagithub@gmail.com>
2024-12-17 18:45:18 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { BsChatTextFill } from "react-icons/bs";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="background-container">
-        <h1 className="thought">What's on your mind today, friend?</h1>
-        <div className="textbox">
-          <input type="textbox__text" />
-        </div>
+        <h1 className="title">THE WALL OF THOUGHTS</h1>
+        <form className="textbox">
+          <div className="textbox__name">
+            <label className="textbox__label" htmlFor="">Enter your name:</label>
+            <input className="textbox__text"  type="text" placeholder="here" />
+          </div>
+          <textarea
+            className="textbox__thought"
+            placeholder="What's on your mind, nigga?"
+          />
+        </form>
       </div>
     </div>
   );
