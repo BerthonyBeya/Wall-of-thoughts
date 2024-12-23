e074cb28570d438d2aa1ee015f06dafbe3441489
Berthony Beya <berthonybeyagithub@gmail.com>
2024-12-23 17:36:54 +0530

    delete logo added

--- src/components/ThoughtCard/ThoughtCard.jsx
+++ src/components/ThoughtCard/ThoughtCard.jsx
@@ -1,3 +1,4 @@
+import { FaDeleteLeft } from "react-icons/fa6";
 import "./ThoughtCard.css";
 
 const ThoughtCard = () => {
@@ -5,12 +6,10 @@ const ThoughtCard = () => {
     <div className="thought-card">
       <div>Card 1</div>
       <div>
+        <FaDeleteLeft className="thought-card__logo"/>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui voluptate
         velit deserunt veniam dignissimos consequatur necessitatibus! Itaque
         accusantium veritatis incidunt eum sint! Voluptatibus vel nisi ipsum,
-        eum aspernatur iste fugiat porro commodi reprehenderit rerum dicta atque
-        architecto a possimus deleniti maxime facere nostrum debitis in quaerat?
-        Similique, alias corrupti? Optio.
       </div>
       <div>Card 3</div>
       <div>Card 4</div>
